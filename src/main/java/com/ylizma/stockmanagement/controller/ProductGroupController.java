364c5d2a73a8d3249e5f78311bc8d8da7b3e6024
ylizma <youssefamzil98@gmail.com>
2020-09-04 13:14:09 +0100

    adding supplier Rest Controller

--- src/main/java/com/ylizma/stockmanagement/controller/ProductGroupController.java
+++ src/main/java/com/ylizma/stockmanagement/controller/ProductGroupController.java
@@ -7,34 +7,34 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/productgroup")
+@RequestMapping("/productgroups")
 public class ProductGroupController {
 
     @Autowired
     ProductGroupService productGroupService;
 
     @GetMapping("/all")
-    java.util.List<ProductGroupDetails> getAllProductGroups() {
+    public java.util.List<ProductGroupDetails> getAllProductGroups() {
         return productGroupService.findAll();
     }
 
     @PostMapping("/add")
-    ResponseEntity<Object> saveProductGroup(@RequestBody ProductGroupDetails productGroup) {
+    public ResponseEntity<Object> saveProductGroup(@RequestBody ProductGroupDetails productGroup) {
         return productGroupService.save(productGroup);
     }
 
     @GetMapping("/{code}")
-    ProductGroupDetails getProductGroupByCode(@PathVariable String code) {
+    public ProductGroupDetails getProductGroupByCode(@PathVariable String code) {
         return productGroupService.findByCode(code);
     }
 
     @PutMapping("/{code}")
-    ResponseEntity<Object> updateProductGroup(@RequestBody ProductGroupDetails product, @PathVariable String code) {
+    public ResponseEntity<Object> updateProductGroup(@RequestBody ProductGroupDetails product, @PathVariable String code) {
         return productGroupService.update(product, code);
     }
 
     @DeleteMapping("/{code}")
-    ResponseEntity<Object> deleteProductGroup(@PathVariable String code) {
+    public ResponseEntity<Object> deleteProductGroup(@PathVariable String code) {
         return productGroupService.delete(code);
     }
 }
---
+++ src/main/java/com/ylizma/stockmanagement/controller/SupplierController.java
@@ -0,0 +1,41 @@
+package com.ylizma.stockmanagement.controller;
+
+import com.ylizma.stockmanagement.domain.SupplierDetails;
+import com.ylizma.stockmanagement.service.SupplierService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/suppliers")
+public class SupplierController {
+
+    @Autowired
+    SupplierService supplierService;
+
+    @GetMapping("/{id}")
+    public SupplierDetails findSupplierById(@PathVariable Long id){
+        return supplierService.findById(id);
+    }
+
+    @GetMapping("/all")
+    public java.util.List<SupplierDetails> getAllSuppliers(){
+        return supplierService.findAll();
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<Object> saveSupplier(@RequestBody SupplierDetails supplier){
+        return supplierService.save(supplier);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Object> updateSupplier(@RequestBody SupplierDetails supplier,@PathVariable Long id){
+        return supplierService.update(supplier,id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteSupplier(@PathVariable Long id){
+        return supplierService.delete(id);
+    }
+
+}
